053afda86b37948afab87d52ca58374ec673dd6a
Donato-Carola <don94@live.it>
2023-11-15 19:13:37 +0100

    stile per farlo diventare un po' piacevole alla vista :)

--- esercizio1-Dadi/js/script.js
+++ esercizio1-Dadi/js/script.js
@@ -9,8 +9,10 @@ const richiamo = document.getElementById('winner');
 const pcElem = document.createElement('h2');
 richiamo.appendChild(pcElem);
 
-const userElem = document.createElement('h3');
+
+const userElem = document.createElement('h2');
 richiamo.appendChild(userElem);
+
 
 const number = document.createElement('h1');
 richiamo.appendChild(number);
@@ -22,13 +24,16 @@ richiamo.appendChild(number);
 
 const clickGame=document.querySelector('button')
 
+
 clickGame.addEventListener("click",
 function(){
 const numUser = Math.floor (Math.random() * 6) + 1;
-
+pcElem.className = 'pc';
+userElem.className = 'user';
                              //**stampo in console il numero del giocatore */
 console.log(`il numero dell'utente è: ${numUser}`);
-document.querySelector('h3').innerHTML=`USER: ${numUser}`;
+document.querySelector('h2.user').innerHTML=`USER: ${numUser}`;
+
 
                              //**creo una variabile dove contenere il numero random del pc */
 const numPc = Math.floor (Math.random() * 6) + 1;
@@ -37,12 +42,14 @@ console.log(`il numero del computer è: ${numPc}`);
 document.querySelector('h2').innerHTML=`PC: ${numPc}`;
 
 
+
                              // ** creo un for per iniziare il gioco ogni volta che aggiorniamo la pagina, secondo il mio ragionamento basta una volta sola quindi per questo i<1*/
 for (let i=0; i<1; i++){
                              //* se il numero del giocatore è maggiore vince */
     if(numUser>numPc){
         console.log(`Il vincitore è il giocatore yeeeeeeeeeeeeeee`);
         document.querySelector('h1').innerHTML=`HAI VINTO <br> <img src="img/winner.jpeg">`;
+
                              //* altrimeni se il numero del pc è maggiore vince quest'ultimo    
     }else if(numPc>numUser){
         console.log(`il vincitore è il computer buuuuu`);
