2ea9b5c03de4ddbcc252fc6343185dd5e8d628c5
Donato-Carola <don94@live.it>
2023-11-15 18:24:39 +0100

    esercizio 2 con stampa a video, con l'email e msg

--- esercizio2-email/js/script.js
+++ esercizio2-email/js/script.js
@@ -7,34 +7,46 @@ Se dobbiamo confrontare qualcosa che "cosa" ci serve?*/
 
                                                             //** chiedere come input all'utente la sua email, quindi creo l'input nell'html */
 
+const risposta = document.getElementById('risultato');
+const text = document.createElement('h1')
+risposta.appendChild(text);
+
+                                                            
 const inputUser = document.querySelector("button");
 
                                                             //** poi in js bisogna creare un array dove inseriamo delle email per confrontarle con l'input (EMAIL)  dell'utente*/
 const arrayEmail = ['don94@live.it', 'MarioCorre@ciambella.com'];
 
+let found = false;
 
                                                             //**aggiungo un evento, cliccando sul button mi fa il controllo dell'email */
 inputUser.addEventListener("click",
 function(){
-    console.log(arrayEmail)
+ //   console.log(arrayEmail)
 
                                                             //**input dell'utente usando .value cosi possiamo usare il valore inserito all'interno dell'elemento input  */
     const emailUser = document.getElementById('input').value;
-    console.log(emailUser)
+   // console.log(emailUser)
     
     
 
     for (let i=0; i<arrayEmail.length; i++){
        const elemento = arrayEmail[i];
-        console.log(i,elemento)
+      //  console.log(i,elemento)
   
          if(emailUser === elemento){
-        console.log('email è giusta continua a navigare');
-    }else{
-       console.log(`Riscrivi l'email perchè l'email è sbagliata`);
-       
+            found = true;
+          //  console.log('email è giusta continua a navigare');
+        }
     }
-  }
+
+    if(found ) {
+        console.log('email trovata');
+        document.querySelector('h1').innerHTML = `email ${emailUser}  trovata`
+    } else if (found === false || emailUser === '') {
+        console.log('email non trovata')
+        document.querySelector('h1').innerHTML = `email ${emailUser} non trovata`
+    }
 
     //** Inizio il controllo */
    
