cd3711d15e5ad0f77de4222a53241ad0d27d35c4
Donato-Carola <don94@live.it>
2023-11-15 16:09:01 +0100

    aggiunto l' IF per verficare se l'email nell'array è uguale o no all'email inserita nell'input

--- esercizio2-email/js/script.js
+++ esercizio2-email/js/script.js
@@ -9,16 +9,31 @@ Se dobbiamo confrontare qualcosa che "cosa" ci serve?*/
 
 const inputUser = document.querySelector("button");
 
+//** poi in js bisogna creare un array dove inseriamo delle email per confrontarle con l'input (EMAIL)  dell'utente*/
+const arrayEmail = ['don94@live.it', 'MarioCorre@ciambella.com'];
+
+//**aggiungo un evento, cliccando sul button mi fa il controllo dell'email */
 inputUser.addEventListener("click",
 function(){
+
+    //**input dell'utente usando .value cosi possiamo usare il valore all'interno dell'elemento input inserito */
     const emailUser = document.getElementById('input').value
+    console.log(emailUser)
+
+
+    //** Inizio il controllo */
+    if(emailUser == arrayEmail){
+        console.log('email è giusta continua a navigare');
+    }else{
+       console.log(`Riscrivi l'email perchè l'email è sbagliata`);
+    }
 })
 
 
 
 
 
-//** poi in js bisogna creare un array dove inseriamo delle email per confrontarle con l'input (EMAIL)  dell'utente*/
+
 
 //** stampare poi a schermo l'esito del controllo */
 //** Se è uguale a quella che c'è nell'array ok */
