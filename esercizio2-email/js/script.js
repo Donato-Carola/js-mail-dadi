dc820a188c292f0ea047921e9d382733a27d200f
Donato-Carola <don94@live.it>
2023-11-15 17:07:56 +0100

    problema sul comparare l'input con l'array

--- esercizio2-email/js/script.js
+++ esercizio2-email/js/script.js
@@ -5,28 +5,39 @@ Prima di partire a scrivere codice poniamoci qualche domanda:
 Che ci sia un array da qualche parte?
 Se dobbiamo confrontare qualcosa che "cosa" ci serve?*/
 
-//** chiedere come input all'utente la sua email, quindi creo l'input nell'html */
+                                                            //** chiedere come input all'utente la sua email, quindi creo l'input nell'html */
 
 const inputUser = document.querySelector("button");
 
-//** poi in js bisogna creare un array dove inseriamo delle email per confrontarle con l'input (EMAIL)  dell'utente*/
+                                                            //** poi in js bisogna creare un array dove inseriamo delle email per confrontarle con l'input (EMAIL)  dell'utente*/
 const arrayEmail = ['don94@live.it', 'MarioCorre@ciambella.com'];
 
-//**aggiungo un evento, cliccando sul button mi fa il controllo dell'email */
+
+                                                            //**aggiungo un evento, cliccando sul button mi fa il controllo dell'email */
 inputUser.addEventListener("click",
 function(){
+    console.log(arrayEmail)
 
-    //**input dell'utente usando .value cosi possiamo usare il valore all'interno dell'elemento input inserito */
-    const emailUser = document.getElementById('input').value
+                                                            //**input dell'utente usando .value cosi possiamo usare il valore inserito all'interno dell'elemento input  */
+    const emailUser = document.getElementById('input').value;
     console.log(emailUser)
+    
+    
 
-
-    //** Inizio il controllo */
-    if(emailUser == arrayEmail){
+    for (let i=0; i<arrayEmail.length; i++){
+       const elemento = arrayEmail[i];
+        console.log(i,elemento)
+  
+         if(emailUser === elemento){
         console.log('email è giusta continua a navigare');
     }else{
        console.log(`Riscrivi l'email perchè l'email è sbagliata`);
+       
     }
+  }
+
+    //** Inizio il controllo */
+   
 })
 
 
